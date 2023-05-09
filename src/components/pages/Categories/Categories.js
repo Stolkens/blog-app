51cdc065d04a4574c3b9d9491d460a34c3eb1031
Stolkens <92957952+Stolkens@users.noreply.github.com>
2023-05-09 14:29:10 +0200

    Add list of categories

--- src/components/pages/Categories/Categories.js
+++ src/components/pages/Categories/Categories.js
@@ -1,6 +1,8 @@
 import { useSelector } from "react-redux";
 import { getCategories } from "../../../redux/categoriesRedux";
 import { ListGroup } from "react-bootstrap";
+import PageTitle from "../../views/PageTitle/PageTitle";
+import styles from './Categories.module.scss'
 
 
 
@@ -10,10 +12,13 @@ import { ListGroup } from "react-bootstrap";
   const categories = useSelector(getCategories)
 
   return (
-    <ListGroup>
-      {categories.map(category=><ListGroup.Item key={category}>{category}</ListGroup.Item>)}
-  
-    </ListGroup>
+    <div className={styles.categories}>
+      <PageTitle>All categories</PageTitle>
+      
+        <ListGroup as="ul" className={styles.list}>
+          {categories.map(category=><ListGroup.Item as="li" action href={"#" + category} key={category}>{category}</ListGroup.Item>)}
+        </ListGroup>
+      </div>
   )
  };
 
